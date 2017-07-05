1c14d7f9246b9c8517c370ed1b5bc28a55d90cf4
tomEdwards <tomo12321@hotmail.com>
2017-07-05 17:28:59 +1200

    Updated items description

--- src/backpack/Item.java
+++ src/backpack/Item.java
@@ -1,9 +1,13 @@
 package backpack;
 
-//This class should deal wih items. Items can be picked up and equipped. At this stage each Item should simply have a name
-// and a passive bonus that they can apply when they are equipped (for example +10 health etc). In future items should all 
-// have a picture, a small description describing what they do and potentially more complicated effects such consumable items,
-// items which add abilities and quest items.
+/**
+ * @author tomo1_000
+ *
+ *         This class should deal with items. Items can be picked up and equipped. At this stage each Item should simply
+ *         have a name and a passive bonus that they can apply when they are equipped (for example +10 health etc). In
+ *         future, items should all have a picture, a small description describing what they do and potentially more
+ *         complicated effects such consumable items, items which add abilities and quest items.
+ */
 public abstract class Item {
 
 	public Item() {

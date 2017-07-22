d69f9a9bce445c6a283c351a717a8440fce79d6a
Thomas Edwards <tomo12321@hotmail.com>
2017-07-22 20:58:48 +1200

    Added comments

--- src/map/Camera.java
+++ src/map/Camera.java
@@ -1,11 +1,15 @@
 package map;
 
+/**
+ * @author James Watt
+ *
+ */
 public class Camera {
 	int width;
 	int height;
 	Map m;
-	
-	public Camera(int width,int height) {
+
+	public Camera(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}

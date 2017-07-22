9cd80d291724fc9ae755c613a9165d20c7762473
Thomas Edwards <tomo12321@hotmail.com>
2017-07-22 20:56:24 +1200

    Added Equipable items comments

--- src/items/EquipableItem.java
+++ src/items/EquipableItem.java
@@ -2,6 +2,12 @@ package items;
 
 import player.Player;
 
+/**
+ * Means that the item can be equipped.
+ * 
+ * @author Thomas Edwards
+ *
+ */
 public interface EquipableItem extends Item {
 
 	public void applyEffect(Player p);

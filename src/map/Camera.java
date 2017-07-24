7dd6e09d5e33ef4f93852b240d6eabd8e4409478
James Watt <james.watt529@hotmail.co.nz>
2017-07-24 19:25:35 +1200

    Moving test gui from map to camera

--- src/map/Camera.java
+++ src/map/Camera.java
@@ -1,7 +1,14 @@
 package map;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Camera {
 	private int pxwidth;
@@ -10,6 +17,7 @@ public class Camera {
 	private int positionX;
 	private int positionY;
 	private ArrayList<ArrayList<Tile>> tileMap;
+	public JPanel drawingPanel;
 
 	public Camera(int width, int height, int posX, int posY, Map m) throws Exception {
 		if (width % 2 > 0 || height % 2 > 0) {
@@ -22,6 +30,7 @@ public class Camera {
 		this.positionY = posY;
 		this.map = m;
 		tileMap = this.map.getTileMap();
+		this.setupTestGUI();
 
 	}
 
@@ -55,5 +64,44 @@ public class Camera {
 
 		}
 	}
+
+
+		public void setupTestGUI() {
+			JFrame frame = new JFrame();// all java swing desktop GUI's must have a JFrame
+
+			// this is our main drawingPanel
+			drawingPanel = new JPanel() {
+				@Override
+				// this is the redraw method, it is called everytime drawingPanel.repaint() is
+				// called
+				public void paintComponent(Graphics g) {
+					render(g);
+					// here we can call the render method, or whatever drawing method you want
+					// render(g,bla,bla,bla,bla);
+				}
+			};
+
+			frame.add(drawingPanel);// if we are only adding a single thing to our frame, we can do it like this.
+
+			// means that the programme will end when the frame is closed
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			frame.setSize(640, 900);// sets the size of the frame
+			frame.setVisible(true);// shows the frame
+
+		}
+		
+		public static void main(String[] args) throws Exception {
+			try {
+				BufferedImage back = ImageIO.read(new File("MapTextures/simplemapback.png"));
+				BufferedImage obj = ImageIO.read(new File("MapTextures/simplemapobj.png"));
+				BufferedImage col = ImageIO.read(new File("MapTextures/simplemapcol.png"));
+				Map m = new Map(back, obj, col);
+				Camera c = new Camera(100,100,0,0,m);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 
 }
